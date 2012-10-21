af8833b8563db02cda749fa8af76163b7c755195
Josh Allen <axlecrusher@gmail.com>
2012-10-21 12:21:02 -0400

    copyright and add sql to check for existing path

--- scripts/fastIndexMedia.pl
+++ scripts/fastIndexMedia.pl
@@ -17,6 +17,9 @@ $dbh->do("SET NAMES UTF8")
 	or die "could not use UTF8\n";
 #$dbo->{'mysql_enable_utf8'} = 1;
 
+my $checkExisting = PrepareStatement("select media_no from media_table where path=?");
+
+
 print "Recursively indexing media in current directory\n";
 
 $path = '../www';
@@ -63,6 +66,17 @@ sub ReapProcess
 	}
 	
 #	$SIG{CHLD} = \&ReapProcess;
+}
+
+sub GetExistingMediaNo
+{
+	my ($path) = @_;
+	$checkExisting->execute($path) or die("Could not insert path");
+	if (my @row = $qe->fetchrow_array)
+	{
+		return $row[0];
+	}
+	return undef;
 }
 
 sub ForkProcess
@@ -333,3 +347,23 @@ sub PrepareStatement($sql)
 
 	return $x;
 }
+
+#Copyright (c) 2012 Joshua Allen
+#
+#Permission is hereby granted, free of charge, to any person obtaining a copy of
+#this software and associated documentation files (the "Software"), to deal in
+#the Software without restriction, including without limitation the rights to
+#use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
+#of the Software, and to permit persons to whom the Software is furnished to do
+#so, subject to the following conditions:
+#
+#The above copyright notice and this permission notice shall be included in all
+#copies or substantial portions of the Software.
+#
+#THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+#IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+#FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+#AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+#LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+#OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+#SOFTWARE.
